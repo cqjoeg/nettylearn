5522ab78a5c816db8a9d4c069a93c1afbf697bfe
Joeg <2956328921@qq.com>
2017-08-15 18:11:08 +0800

    first commit

---
+++ src/main/java/p/joe/netty/TimeClient.java
@@ -0,0 +1,63 @@
+package p.joe.netty;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by CQ on 2017/8/15.
+ */
+public class TimeClient {
+    public static void main(String[] args) {
+        String[] args1 = {"8080"};
+        int port = 8080;
+        if (args1 != null && args1.length > 0) {
+            try {
+                port = Integer.valueOf(args1[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Socket socket = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            socket = new Socket("127.0.0.1", port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            out.println("QUERY TIME ORDER");
+            System.out.println("Send order 2 server succeed.");
+            String resp = in.readLine();
+            System.out.println("Now is:" + resp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (out != null) {
+                out.close();
+                out = null;
+            }
+
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                in = null;
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println();
+                socket = null;
+            }
+        }
+
+    }
+}
---
+++ src/main/java/p/joe/netty/TimeServer.java
@@ -0,0 +1,44 @@
+package p.joe.netty;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TimeServer {
+
+    public static void main(String[] args) throws IOException {
+        String[] args1 = {"8080"};
+        int port = 8080;
+        if (args1 != null && args1.length > 0) {
+            try {
+                port = Integer.valueOf(args1[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(port);
+            System.out.println("The time server is start in port:" + port);
+            Socket socket = null;
+            TimeServerHandlerExecutePool singleExecutor = new TimeServerHandlerExecutePool(50, 10000);
+            while (true) {
+                socket = server.accept();
+                singleExecutor.execute(new TimeServerHandler(socket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (server != null) {
+                System.out.println("The time server is closed!");
+                server.close();
+                server = null;
+            }
+
+        }
+
+
+    }
+}
